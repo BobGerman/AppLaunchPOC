8092d267548f27b7204746ec865cda68dded6721
Bob German <bob@bgtest18.onmicrosoft.com>
2020-05-27 16:16:46 -0400

    Ran yo teams for 1 tab

---
+++ src/app/pocTab/PocTab.ts
@@ -0,0 +1,10 @@
+import { PreventIframe } from "express-msteams-host";
+
+/**
+ * Used as place holder for the decorators
+ */
+@PreventIframe("/pocTab/index.html")
+
+
+export class PocTab {
+}
---
+++ src/app/scripts/client.ts
@@ -0,0 +1,2 @@
+// Automatically added for the pocTab tab
+export * from "./pocTab/PocTab";
